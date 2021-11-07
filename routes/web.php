6eb1e75dba497fef3ff55e38d213ff4f289d3aaf
Tiago Pereira <tiagopatureba@gmail.com>
2021-11-07 10:54:49 -0300

    Start example CRUD

--- routes/web.php
+++ routes/web.php
@@ -3,16 +3,20 @@
 use Illuminate\Support\Facades\Route;
 
 /*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+ * Possibilidades com rotas:
+ * Rotas com parâmetros (por padrão obrigatórios)
+ * Rotas com parâmetros opcionais (interrogação)
+ * Rotas com regras (regex - expressões regulares dentro de clausulas Where)
+ * Rotas agrupadas (prefix / group)
+ * Rotas nomeadas (->name())
+ * Redirecionamento de Rotas
+ * Rotas que não são GET(todas as outras) têm que ter tratamento CSRF para a segurança da aplicação
+ * (que gera um token no lado do Form e faz o check no Backend - para garantir que não foi feito injection e que a fonte é realmente do form)
+ * Rotas associadas (resource - porém controller têm que estar no padrão Laravel)
+ */
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('marcas', "App\Http\Controllers\BrandController");
